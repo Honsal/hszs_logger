96ec376f04c3b7e14eb45034d2c775d9802c7a3f
Honsal <honjasalayo@gmail.com>
2017-10-05 14:19:43 +0900

    Initial commit

---
+++ lua/hslog/hslog.lua
@@ -0,0 +1,47 @@
+HSLog = {}
+
+HSLog.ulxLog = function(text)
+	if (istable(ulx) and isfunction(ulx.logString)) then
+		ulx.logString(text)
+	end
+end
+
+HSLog.a = function(text)
+	-- HSLog.LastMessage = text
+	text = "[HSLog] " .. text
+	MsgC(Color(255, 255, 0), text .. "\n")
+	PrintMessage(HUD_PRINTTALK, text)
+	
+	HSLog.ulxLog(text)
+end
+
+HSLog.d = function(sender, text, alert)
+	-- HSLog.LastMessage = text
+	text = (sender and (sender .. " @ ") or "") .. text
+	text = "[HSLog@Debug] " .. text
+	MsgC(Color(0, 255, 0), text .. "\n")
+	HSLog.ulxLog(text)
+	if alert then
+		PrintMessage(HUD_PRINTTALK, text)
+	end
+end
+
+HSLog.e = function(sender, text)
+	-- HSLog.LastMessage = text
+	text = (sender and (sender .. " @ ") or "") .. text
+	MsgC(Color(255, 0, 0), "[HSLog@Error] " .. text .. "\n")
+	HSLog.ulxLog(text)
+end
+
+HSLog.w = function(sender, text)
+	-- HSLog.LastMessage = text
+	text = (sender and (sender .. " @ ") or "") .. text
+	MsgC(Color(180, 0, 0), "[HSLog@Warning] " .. text .. "\n")
+	HSLog.ulxLog(text)
+end
+
+-- HSLog.LastMessage = ""
+
+hook.Add("PlayerConnect", "LogIPAddr", function(pl, ip)
+	HSLog.a(pl .. "님의 IP: " .. ip)
+end)
